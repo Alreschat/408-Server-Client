777918426a03b58e2609f3be9eb5933656bd176b
Umut Zenger <zengerumut@gmail.com>
2017-11-26 03:27:07 +0300

    First step, missing disconnect detection and has -wrong- implementation of unique user names

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDisc = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.listClients = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(56, 128);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(56, 44);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.Text = "8910";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(56, 12);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 7;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Host:";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(209, 44);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(285, 226);
+            this.txtStatus.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Clients";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Status";
+            // 
+            // btnDisc
+            // 
+            this.btnDisc.Location = new System.Drawing.Point(56, 176);
+            this.btnDisc.Name = "btnDisc";
+            this.btnDisc.Size = new System.Drawing.Size(75, 23);
+            this.btnDisc.TabIndex = 15;
+            this.btnDisc.Text = "Disconnect";
+            this.btnDisc.UseVisualStyleBackColor = true;
+            this.btnDisc.Click += new System.EventHandler(this.btnDisc_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(56, 70);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Name";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(44, 288);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(605, 76);
+            this.txtMessage.TabIndex = 18;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(574, 380);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 19;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // listClients
+            // 
+            this.listClients.FormattingEnabled = true;
+            this.listClients.Location = new System.Drawing.Point(531, 47);
+            this.listClients.Name = "listClients";
+            this.listClients.Size = new System.Drawing.Size(118, 225);
+            this.listClients.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 425);
+            this.Controls.Add(this.listClients);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnDisc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDisc;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox listClients;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using SimpleTCP;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        
+        public void sendList()
+        {
+            foreach (var item in userbase)
+            {
+                item.Key.GetStream().Write(Encoding.UTF8.GetBytes("/list "), 0, 6);
+                foreach (var items in userbase)
+                {
+                    item.Key.GetStream().Write(Encoding.UTF8.GetBytes(items.Value), 0, items.Value.Length);
+                    item.Key.GetStream().Write(Encoding.UTF8.GetBytes(" "), 0, 1);
+                }
+            }
+            
+        }
+
+        public Form1()//Starts it up
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer servertcp; //server tcp
+        string uname; //Username of the current client
+        Dictionary<System.Net.Sockets.TcpClient, string> userbase = new Dictionary<System.Net.Sockets.TcpClient, string>(); //Holds username as value, tcpclient as key
+
+        private void Form1_Load(object sender, EventArgs e) // Create tcp server, configure it, disable stop button
+        {
+            servertcp = new SimpleTcpServer();
+            servertcp.Delimiter = 0x13;
+            servertcp.StringEncoder = Encoding.UTF8;
+            servertcp.DataReceived += Server_DataReceived;
+            btnStop.Enabled = false;
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e) // when data is recieved
+        {
+            string command = "";
+            string delname = "";
+            if (e.MessageString.Length >= 6) // so it doesn't crush when length < 6
+            {
+               command = e.MessageString.Substring(0, 5);
+            }
+            if (command.Equals("/nick")) // assign nick
+            {
+                uname = e.MessageString.Substring(6);
+                while (userbase.ContainsValue(uname))
+                {
+                    uname = uname + "1";
+                }
+                userbase.Add(e.TcpClient, uname);
+                listClients.Invoke((MethodInvoker)delegate ()
+                {
+                    listClients.Items.Add(uname);
+                });
+                txtStatus.Invoke((MethodInvoker)delegate ()
+                {
+                    txtStatus.Text += uname + " just connected\n" + System.Environment.NewLine;
+                });
+
+                sendList();
+            }
+            else if (command.Equals("/deln")) // disconnect, remove nick
+            {
+                delname = userbase[e.TcpClient];
+                userbase.Remove(e.TcpClient);
+                listClients.Invoke((MethodInvoker)delegate ()
+                {
+                    listClients.Items.Remove(delname);
+                });
+                txtStatus.Invoke((MethodInvoker)delegate ()
+                {
+                    txtStatus.Text += delname + " just disconnected" + System.Environment.NewLine;
+                });
+                sendList();
+            }
+            else
+            {
+                txtStatus.Invoke((MethodInvoker)delegate ()
+                {
+                    txtStatus.Text += e.MessageString;
+                    e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+
+                });
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) // start server
+        {
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            txtStatus.Text += "Server starting..." + System.Environment.NewLine;
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text);
+            servertcp.Start(ip, Convert.ToInt32(txtPort.Text));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e) // stop server
+        {
+            servertcp.Stop();
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+        }
+    }
+}
